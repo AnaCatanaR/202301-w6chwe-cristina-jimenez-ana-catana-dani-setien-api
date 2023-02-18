17fb989582a924aa4d1477bde0ec6c139dab0b32
Dani Setien <dani.setien@gmail.com>
2023-02-18 23:13:03 +0100

    Install cors & enable it for all requests

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,9 +4,15 @@ import express from "express";
 import { robotsRouter } from "./router/robotsRouter.js";
 import { notFoundError } from "./middlewares/errorMiddlewares.js";
 import generalError from "./middlewares/generalError/generalError.js";
+import cors from "cors";
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+const corsOptions = {
+  origin: "trustedwebsite.com",
+};
+app.use(cors(corsOptions));
 
 app.use("/robots", robotsRouter);
 

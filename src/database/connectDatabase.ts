fa6e330cbf954d9aeb08769baffe6e3ce0da1e32
Cristina <cristinajmartin98@gmail.com>
2023-02-18 21:25:32 +0100

    Change toJSON default method to transform _id property to id

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -4,6 +4,13 @@ const connectDatabase = async () => {
   try {
     mongoose.set("strictQuery", false);
 
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform(doc, converted) {
+        delete converted._id;
+      },
+    });
+
     await mongoose.connect(process.env.ROBOTS_DATABASE!);
   } catch (error) {
     throw new Error("Error connecting to the database");

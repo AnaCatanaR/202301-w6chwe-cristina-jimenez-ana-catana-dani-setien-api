88de3228c43dc35910821c599158ac2caa00605e
Cristina <cristinajmartin98@gmail.com>
2023-02-18 14:35:57 +0100

    Add feedback about connecting to database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "./loadEnvironments.js";
 import "./middlewares/middlewares.js";
-
 import { app, startServer } from "./listeners/startServer.js";
 import { type Request, type Response } from "express";
+import connectDatabase from "./database/connectDatabase.js";
 
 export const port = process.env.PORT ?? 4001;
 
 startServer(+port);
 
 app.use("/robots", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Site in development, come back later!" });
+  res.status(200).json({});
 });
+
+await connectDatabase();

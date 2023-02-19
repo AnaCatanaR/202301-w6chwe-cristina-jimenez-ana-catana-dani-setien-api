abdeba1dbcbf97fbca1d30fca19cd5f80065763b
Dani Setien <dani.setien@gmail.com>
2023-02-19 12:02:42 +0100

    Switch middlewares order to prioritize cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,13 +6,13 @@ import { notFoundError } from "./middlewares/errorMiddlewares.js";
 import generalError from "./middlewares/generalError/generalError.js";
 import cors from "cors";
 
-app.use(morgan("dev"));
-app.use(express.json());
-
 const corsOptions = {
   origin: "trustedwebsite.com",
 };
 app.use(cors(corsOptions));
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.use("/robots", robotsRouter);
 

f0fe9512fdb89d37565d6626b7110312f1ce6d59
Cristina <cristinajmartin98@gmail.com>
2023-02-19 17:54:37 +0100

    Add create endpoint

--- src/server/router/robotsRouter.ts
+++ src/server/router/robotsRouter.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getRobots, getRobotById } from "../controllers/robotsController.js";
+import {
+  getRobots,
+  getRobotById,
+  createRobot,
+} from "../controllers/robotsController.js";
 
 export const robotsRouter = Router();
 
 robotsRouter.get("/", getRobots);
 robotsRouter.get("/:idRobot", getRobotById);
+robotsRouter.post("/create", createRobot);
